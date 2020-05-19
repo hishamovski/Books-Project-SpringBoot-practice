e0d14ebec77ca4ef9f8077fb794ea293d918c569
hishamovski <hisham.boualam@gmail.com>
2020-05-19 17:23:48 -0400

    Add Controller
    Create the controller and the routes for crud requests.

---
+++ src/main/java/com/WannaBe/Project/API/BookController.java
@@ -0,0 +1,48 @@
+package com.WannaBe.Project.API;
+
+import com.WannaBe.Project.Model.Book;
+import com.WannaBe.Project.Service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RequestMapping("api/v1/books")
+@RestController
+public class BookController {
+
+    private BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PostMapping
+    public void addBook(@NonNull @RequestBody Book book) {
+        bookService.addBook(book);
+    }
+
+    @GetMapping
+    public List<Book> getBooks(){
+        return bookService.getBooks();
+    }
+
+    @GetMapping(path = "/{id}")
+    public Book getBook(@PathVariable("id") UUID id){
+        return bookService.getBook(id)
+                .orElse( null);
+    }
+    @DeleteMapping(path = "/{id}")
+    public void deleteBook(@PathVariable("id") UUID id) {
+        bookService.deleteBook(id);
+    }
+
+    @PutMapping(path = "/{id}")
+    public void updateBook(@PathVariable("id") UUID id, @NonNull @RequestBody Book book){
+        bookService.updateBook(id, book);
+    }
+}
